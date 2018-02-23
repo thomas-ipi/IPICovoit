e9166c0a4165fda1e80995b62fb214a88a0605af
tevak <tevak@172.29.49.255>
2018-02-23 11:14:21 +0100

    Verif inscription JS OK

--- IPICovoit/WebContent/Assets/script.js
+++ IPICovoit/WebContent/Assets/script.js
@@ -11,44 +11,68 @@ function closeInscription()
 
 function verifInscription()
 {
+	
 	/* Vérification nom + prénom : mini 2 caractères */
 	var nomInscription = $('#nomInscription').val();
 	var prenomInscription = $('#prenomInscription').val();
 	if (nomInscription.length <=1)
 	{
-		$('#nomInscription').css('border', '4px solid red');
-		return false;
-	} else
-		{
-			return true;
-		}
+		$('#nomInscription').css('border', '3px solid red');
+		errorNom = true;
+	} else {errorNom = false};
 	
-	
+	if (prenomInscription.length <=1)
+	{
+		$('#prenomInscription').css('border', '3px solid red');
+		errorPrenom = true;
+	} else {errorPrenom = false};
 	
 	
 	/* Vérification téléphone : 10 chiffres + Que des nombres */
 	var telInscription = $('#telInscription').val();
 	if (((/^[0-9]*$/.test(telInscription)) == true)){
-		if (telInscription.length == 10)
+		if (telInscription.length != 10)
 		{
-			return true;
-		} 
-		else
-		{
-			$('#telInscription').css('border', '4px solid red');
-			return false;
-		}
+			$('#telInscription').css('border', '3px solid red');
+			errorTel = true;
+		} else {errorTel = false};
 	}
 	else
 	{
-		$('#telInscription').css('border', '4px solid red');
-		return false;
+		$('#telInscription').css('border', '3px solid red');
+		errorTel = true;
 	}
 	
-	
-	/* Vérification si l'extension mail est bien "@campus-igs-toulouse.fr" */
 
+	/* Vérification si l'extension mail est bien "@campus-igs-toulouse.fr" */
 	var mailInscription = $('#mailInscription').val();
-	alert(mailInscription);
+	extension = mailInscription.substring(mailInscription.lastIndexOf("@"));
+	if (extension != '@campus-igs-toulouse.fr')
+	{
+		$('#mailInscription').css('border', '3px solid red');
+		errorMail = true;
+	} else {errorMail = false};
+	
+	
+	/* Vérification si mots de passe identiques*/
+	var mdp1Inscription = $('#mdp1Inscription').val();
+	var mdp2Inscription = $('#mdp2Inscription').val();
+	
+	if (mdp1Inscription != mdp2Inscription)
+	{
+		$('#mdp1Inscription').css('border', '3px solid red');
+		$('#mdp2Inscription').css('border', '3px solid red');
+		errorMdp = true;
+	} else {errorMdp = false};
+	
+	/* Return final */
+	if (errorNom == false && errorPrenom == false && errorTel == false && errorMail == false && errorMdp == false)
+		{
+		return true;
+		}
+	else
+		{
+		return false;
+		}
 	
 }
