8369fcc5375df4a97bbc999364a30acebc15b5c7
Audrey <Audrey@PC-GNOCCHI>
2018-04-20 15:41:38 +0200

    aa

--- IPICovoit/WebContent/Assets/script.js
+++ IPICovoit/WebContent/Assets/script.js
@@ -67,24 +67,13 @@ function getTrajects()
              	}); 
                
                var contentString = '<div id="content">'+
-               '<div id="siteNotice">'+
-               '</div>'+
-               '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+               '<h6 id="firstHeading" class="firstHeading">'+ element[5] + element[4] + '</h6>'+
                '<div id="bodyContent">'+
-               '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-               'sandstone rock formation in the southern part of the '+
-               'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-               'south west of the nearest large town, Alice Springs; 450&#160;km '+
-               '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-               'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-               'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-               'Aboriginal people of the area. It has many springs, waterholes, '+
-               'rock caves and ancient paintings. Uluru is listed as a World '+
-               'Heritage Site.</p>'+
-               '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-               'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-               '(last visited June 22, 2009).</p>'+
+               '<p>'+ element[7] + '</p>'+
+               '<p>'+ element[6] + '</p>'+
+               '<p>'+ element[3] + '</p>'+
                '</div>'+
+               '<button type="button" class="btn btn-outline-success btn-sm" onClick="if (confirm(\'Êtes-vous sûr de vouloir réserver ce trajet?\')) {window.location.assign(\'Reserver?mailconducteur=??&mailpassager=??\')}">Réserver</button>'
                '</div>';
 
            var infowindow = new google.maps.InfoWindow({
