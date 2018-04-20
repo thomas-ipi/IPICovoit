90ab8d0990ab57f6d142f48bc78b1a8b2c444cdc
Thomas BDC <Thomas BDC@Thomas-BDC>
2018-04-20 10:52:06 +0200

    Send Mail

--- IPICovoit/src/com/IPICovoit/Reservation.java
+++ IPICovoit/src/com/IPICovoit/Reservation.java
@@ -3,8 +3,16 @@
 	import java.io.IOException;
 	import java.util.HashMap;
 	import java.util.Map;
-	import java.sql.*;
+import java.util.Properties;
+import java.sql.*;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 	import javax.servlet.annotation.WebServlet;
@@ -20,6 +28,13 @@ public class Reservation extends HttpServlet{
 	private static String VIEW_PAGES_URL = "/";
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			sendMail();
+			request.setAttribute("successMessage", "Le mail a été envoyé.");
+		}catch(MessagingException e) {
+			request.setAttribute("errorMessage", "Le mail n'a pas pu être envoyé");
+		}
+		this.getServletContext().getRequestDispatcher(VIEW_PAGES_URL).include( request, response );
 	}
 
 	/**
@@ -44,6 +59,64 @@ public class Reservation extends HttpServlet{
 			e.printStackTrace();
 			request.setAttribute("errorMessage", "Votre réservation a échouée. Merci de réessayer ultérieurement.");
 		}
+		
 		this.getServletContext().getRequestDispatcher(VIEW_PAGES_URL).include( request, response );
+	}
+	
+	private void sendMail() throws MessagingException{
+		// Recipient's email ID needs to be mentioned.
+	      String to = "totoipodbdc@gmail.com";
+
+	      // Sender's email ID needs to be mentioned
+	      String from = "ipicoivoir@alwaysdata.net";
+
+	      // Assuming you are sending email from localhost
+	      String host = "smtp-ipicoivoir.alwaysdata.net";
+
+	      // Get system properties
+	      Properties properties = System.getProperties();
+
+	      // Setup mail server
+	      properties.setProperty("mail.smtp.host", host);
+	      properties.setProperty("mail.smtp.auth", "true");
+
+
+	      // Get the default Session object.
+	      //Session session = Session.getDefaultInstance(properties);
+	      Session session = Session.getInstance(properties,
+	    		  new javax.mail.Authenticator() {
+	    			protected PasswordAuthentication getPasswordAuthentication() {
+	    				return new PasswordAuthentication("ipicoivoir@alwaysdata.net", "IPICovoit");
+	    			}
+	    		  });
+
+	      try {
+	         // Create a default MimeMessage object.
+	         MimeMessage message = new MimeMessage(session);
+
+	         // Set From: header field of the header.
+	         message.setFrom(new InternetAddress(from));
+
+	         // Set To: header field of the header.
+	         message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+	         // Set Subject: header field
+	         message.setSubject("Vous venez de réserver un trajet chez IPICovoit.");
+
+	         String contenu = "<h1>Merci de votre réservation</h1>"
+	         		+ "Vous pouvez désormais contacter votre conducteur au coordonnées suivantes : <br/>"
+	         		+ "ICI METTRE LES COORDONNEES <br/>"
+	         		+ "<br/>"
+	         		+ "J'aime le veau <br/> "
+	         		+ "Cordialement";
+	         // Now set the actual message
+	         message.setContent(contenu, "text/html; charset=utf-8");
+
+	         // Send message
+	         Transport.send(message);
+	         System.out.println("Sent message successfully....");
+	      } catch (MessagingException mex) {
+	         mex.printStackTrace();
+	      }
 	}
 }
